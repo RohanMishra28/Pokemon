58cd0c489ea56637f7dc42582c8b03c2c45d6d56
Rohan Mishra <146756038+RohanMishra28@users.noreply.github.com>
2024-12-13 02:59:15 +0530

    done

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pokemon from './components/Pokemon'
+
+const App = () => {
+  return (
+    <div>
+      <Pokemon></Pokemon>
+    </div>
+  )
+}
+
+export default App
